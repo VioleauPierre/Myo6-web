5594cf3f5daf0413881ef7e96e8cc2dbcd7c1d7a
VioleauPierre <violeau.p@gmail.com>
2025-01-06 18:46:31 +0100

    Update ajout consignes sur l'interface

--- myo6/pages/addFingerprint.jsx
+++ myo6/pages/addFingerprint.jsx
@@ -3,13 +3,10 @@ import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
-
-  // State for test function inputs
   const [userId, setUserId] = useState('');
   const [sensorId, setSensorId] = useState('');
   const [testSubmissionMessage, setTestSubmissionMessage] = useState('');
 
-  // Test function to submit userId and sensorId
   const handleTestFunctionSubmit = async (event) => {
     event.preventDefault();
 
@@ -47,68 +44,74 @@ export default function Home() {
   return (
     <>
       <Header></Header>
-      {/* <div className="scrollable-content"> */}
       <div className="h-screen w-screen">
         <Navbar></Navbar>
         <hr className="w-full h-[4px] bg-beige"></hr>
-        <div className='flex  min-h-[calc(100%-68px)] bg-gray-300 h-auto '>
-          {/* <SideBar></SideBar> */}
-          <div id="main_code" className="h-full  w-full ">
-            <div className="w-full p-2 ">
-              <div className="flex bg-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center justify-items-center h-fit">
-                <div className="text-xl font-bold text-[#082431]">
-                  Enregistrement empreinte
-                </div>
+        <div className="flex min-h-[calc(100%-68px)] bg-gray-300 h-auto">
+          <div id="main_code" className="h-full w-full">
+            {/* Section pour le texte explicatif */}
+            <div className="w-full p-4 bg-white rounded-lg shadow-md border border-gray-300 mb-4">
+              <h2 className="text-xl font-bold text-[#082431] mb-2">
+                Instructions pour l'utilisateur
+              </h2>
+              <p className="text-gray-700 mb-2">
+                Veuillez suivre les étapes ci-dessous :
+              </p>
+              <ul className="list-disc pl-5 text-gray-700">
+                <li>Entrez l'<strong>ID Utilisateur</strong> dans le champ prévu à cet effet.</li>
+                <li>Ajoutez l'<strong>ID Capteur</strong> dans le deuxième champ.</li>
+                <li>Si vous avez terminé, cliquez sur le bouton <strong>"Envoyer"</strong> pour valider vos données.</li>
+                <li>Vous recevrez un message de confirmation ou d'erreur après l'envoi.</li>
+                <li>Vérifiez que <strong>le capteur est bien connecté à un réseau</strong> en appuyant sur le <strong>bouton rouge</strong>, si après quelques secondes l'anneau de LED clignote en vert, le capteur est connecté</li>
+                <li>Maintenez le <strong>bouton noir</strong> appuyé pendant + de 2 secondes (l'anneau de LED passe en orange clignotant) pour entrer dans le mode association</li>
+                <li>Posez le doigt à enregistrer sur le capteur d'empreinte jusqu'à ce que l'anneau de LED change de couleur</li>
+              </ul>
+            </div>
+
+            <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2">
+                <p>ID Utilisateur : </p>
+                <input
+                  type="number"
+                  value={userId}
+                  onChange={(e) => setUserId(e.target.value)}
+                  placeholder="Entrez l'ID de l'utilisateur"
+                  className="ml-2 w-full"
+                />
               </div>
+
+              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-4 border-gray-400 p-4">
+                <p>ID Capteur : </p>
+                <input
+                  type="number"
+                  value={sensorId}
+                  onChange={(e) => setSensorId(e.target.value)}
+                  placeholder="Entrez l'ID du capteur"
+                  className="ml-2 w-full"
+                />
+              </div>
+
+              <div className="flex w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                <button
+                  onClick={handleTestFunctionSubmit}
+                  className="bg-sky-600 text-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 w-full"
+                >
+                  Envoyer
+                </button>
+              </div>
+
+              {testSubmissionMessage && (
+                <div
+                  className={`text-white rounded-lg p-2 mt-4 ${
+                    testSubmissionMessage.includes('succès') ? 'bg-green-500' : 'bg-red-500'
+                  }`}
+                >
+                  {testSubmissionMessage}
+                </div>
+              )}
             </div>
-
-            <div className="w-full sm:w-1/2 p-2 justify-center items-center justify-items-center ml-auto mr-auto ">
-              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2  border-gray-400 p-2 justify-center items-center justify-items-center h-full">
-
-            <p>ID Utilisateur : </p>
-            <input
-              type="number"
-              value={userId}
-              onChange={(e) => setUserId(e.target.value)}
-              placeholder="Entrez l'ID de l'utilisateur"
-              className="ml-2 w-full"
-            />
           </div>
-
-          {/* Sensor ID Field */}
-          <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-4 border-gray-400 p-4">
-            <p>ID Capteur : </p>
-            <input
-              type="number"
-              value={sensorId}
-              onChange={(e) => setSensorId(e.target.value)}
-              placeholder="Entrez l'ID du capteur"
-              className="ml-2 w-full"
-            />
-          </div>
-
-          <div className="flex w-1/2 p-2 justify-center items-center justify-items-center ml-auto mr-auto ">
-              <div className="flex bg-sky-600 text-center text-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center justify-items-center h-full">
-          <button
-            onClick={handleTestFunctionSubmit}
-          >
-            Envoyer
-          </button>
-          </div>
-          </div>
-          {/* Submission Message */}
-          {testSubmissionMessage && (
-            <div
-              className={`text-white rounded-lg p-2 mt-4 ${
-                testSubmissionMessage.includes('succès') ? 'bg-green-500' : 'bg-red-500'
-              }`}
-            >
-              {testSubmissionMessage}
-            </div>
-          )}
         </div>
-      </div>
-      </div>
       </div>
     </>
   );
--- myo6/pages/qrcode.jsx
+++ myo6/pages/qrcode.jsx
@@ -1,8 +1,6 @@
-// import withAuth from '../components/withAuth';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
-
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
@@ -16,16 +14,17 @@ function Home() {
   const [password, setPassword] = useState('');
   const [encryption, setEncryption] = useState('');
   const [serverResponse, setServerResponse] = useState('');
+  const [mode, setMode] = useState(''); // Variable d'état pour le mode sélectionné
 
-  // Function to generate Wi-Fi credentials for QR Code
+  // Fonction pour générer les informations Wi-Fi en QR Code
   const handleGenerateQR = () => {
     const wifiCredentials = `WIFI:T:${encryption};S:${ssid};P:${password};H:;`;
     return wifiCredentials;
   };
 
-  // Function to handle the POST request
+  // Fonction pour envoyer les paramètres au serveur
   const handleSendToServer = async () => {
-    const url = 'https://myo6.duckdns.org/api/wifi_update'; // Replace with the actual server URL
+    const url = 'https://myo6.duckdns.org/api/wifi_update'; // Remplacez par l'URL du serveur réel
     const data = {
       sensor_id: sensorId,
       ssid: ssid,
@@ -61,91 +60,233 @@ function Home() {
         <hr className="w-full h-[4px] bg-beige" />
         <div className="flex min-h-[calc(100%-68px)] bg-gray-300 h-auto">
           <div id="main_code" className="h-full w-full">
-            <div className="w-full p-2">
-              <div className="flex bg-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-fit">
-                <div className="text-xl font-bold text-[#082431]">
-                  Générateur de QR Code Wi-Fi
+            {/* Bloc d'information conditionnel */}
+            {!mode && (
+              <div className="w-full p-2 mb-4">
+                <div className="flex bg-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center">
+                  <div className="text-xl font-bold text-[#082431]">
+                    Informations importantes :
+                  </div>
+                </div>
+                <div className="p-4 bg-white border-2 border-gray-400 rounded-lg shadow-lg">
+                  <p>
+                    Choisissez l'une des options suivantes :
+                  </p>
+                    <ul className="list-disc pl-5">
+                      <li>
+                        <strong>Générer un QR Code :</strong> Si vous souhaitez générer un QR Code pour
+                        connecter le capteur à un réseau wi-fi.
+                      </li>
+                      <li>
+                        <strong>Entrer un nouveau réseau (Ne fonctionne que si le capteur est déjà connecté à un réseau):</strong> Si vous souhaitez entrer un nouveau réseau
+                        Wi-Fi pour un capteur via le serveur.
+                      </li>
+                    </ul>
+                  
                 </div>
               </div>
-            </div>
+            )}
 
-            {/* Sensor ID Field */}
+            {/* Sélecteur du mode */}
             <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
               <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
-                <p> ID Capteur: </p>
-                <input
-                  type="number"
-                  value={sensorId}
-                  onChange={(e) => setSensorId(e.target.value)}
-                  placeholder="Entrez l'ID du capteur"
-                  style={{ width: '190px' }}
-                />
-              </div>
-            </div>
-
-            {/* SSID Field */}
-            <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
-              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
-                <p> SSID: </p>
-                <input
-                  type="text"
-                  value={ssid}
-                  onChange={(e) => setSsid(e.target.value)}
-                  placeholder="Saisissez votre réponse"
-                  style={{ width: '190px' }}
-                />
-              </div>
-            </div>
-
-            {/* Password Field */}
-            <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
-              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
-                <p> Mot de passe : </p>
-                <input
-                  type="text"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Saisissez votre réponse"
-                  style={{ width: '190px' }}
-                />
-              </div>
-            </div>
-
-            {/* Encryption Type Dropdown */}
-            <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
-              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
-                <p> Type de cryptage: </p>
+                <p>Choisissez une option:</p>
                 <select
-                  id="encryption"
-                  value={encryption}
-                  onChange={(e) => setEncryption(e.target.value)}
+                  value={mode}
+                  onChange={(e) => setMode(e.target.value)}
+                  style={{ width: '190px' }}
                 >
                   <option value="">Sélectionner</option>
-                  <option value="WPA">WPA</option>
-                  <option value="WEP">WEP</option>
-                  <option value="nopass">Aucun</option>
+                  <option value="generateQR">Générer QR Code</option>
+                  <option value="enterNetwork">Entrer nouveau réseau</option>
                 </select>
               </div>
             </div>
 
-            {/* QR Code Display */}
-            <div className="w-fit p-2 justify-center items-center ml-auto mr-auto">
-              <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
-                <QRCode value={handleGenerateQR()} />
+            {/* Message de guidage en fonction du mode sélectionné */}
+            {mode === 'generateQR' && (
+              <div className="w-full p-2 mb-4">
+                <div className="flex bg-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center">
+                  <div className="text-xl font-bold text-[#082431]">
+                    Générer un QR Code Wi-Fi
+                  </div>
+                </div>
+                <div className="p-4 bg-white border-2 border-gray-400 rounded-lg shadow-lg">
+                  <p>
+                    Pour générer un QR Code Wi-Fi, entrez les informations suivantes :
+                    <ul className="list-disc pl-5">
+                      <li><strong>SSID</strong> : Le nom du réseau Wi-Fi.</li>
+                      <li><strong>Mot de passe</strong> : Le mot de passe de votre réseau.</li>
+                      <li><strong>Type de cryptage</strong> : Choisissez le type de sécurité utilisé par votre réseau (WPA, WEP, etc.). Le plus courant est le WPA</li>
+                      <li><strong>Etape 1</strong> : Renseigner les différents champs.</li>
+                      <li><strong>Etape 2</strong> : Approcher le capteur avec la caméra face au QR Code.</li>
+                      <li><strong>Etape 3</strong> : Appuyer sur le bouton rouge pour déclencher la procédure de détection du QR Code. L'anneau de LED clignote bleu foncé pendant la procédure</li>
+                      <li><strong>Etape 4</strong> : Après quelques secondes, l'anneau de LED clignotera en vert si le capteur est connecté à un réseau et retrouve ensuite son état statique (LED bleu ou verte fixe). La connexion peut prendre un peu plus longtemps.</li>
+                      <li><strong>Test</strong> : Appuyer sur le bouton rouge, après quelques secondes,si l'anneau de LED clignote en vert avant de redevenir statique alors le capteur est bien connecté à un réseau</li>
+                      <li><strong>A savoir</strong> : Il est parfois nécessaire d'éteindre et rallumer le réseau ainsi que le capteur pour que la connexion s'effectue</li>
+                    </ul>
+                  </p>
+                </div>
               </div>
-            </div>
+            )}
 
-            {/* Send to Server Button */}
-            <div className="w-fit p-2 justify-center items-center ml-auto mr-auto">
-              <button
-                onClick={handleSendToServer}
-                className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md"
-              >
-                Envoyer les paramètres au serveur
-              </button>
-            </div>
+            {mode === 'enterNetwork' && (
+              <div className="w-full p-2 mb-4">
+                <div className="flex bg-white rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center">
+                  <div className="text-xl font-bold text-[#082431]">
+                    Entrer un nouveau réseau Wi-Fi
+                  </div>
+                </div>
+                <div className="p-4 bg-white border-2 border-gray-400 rounded-lg shadow-lg">
+                  <p>
+                    Pour entrer un nouveau réseau Wi-Fi pour un capteur, fournissez les informations suivantes :
+                    <ul className="list-disc pl-5">
+                      <li><strong>ID Capteur</strong> : L'identifiant unique de votre capteur.</li>
+                      <li><strong>SSID</strong> : Le nom du réseau Wi-Fi.</li>
+                      <li><strong>Mot de passe</strong> : Le mot de passe du réseau.</li>
+                      <li><strong>Type de cryptage</strong> : Le type de sécurité du réseau (WPA, WEP, etc.).</li>
+                      <li><strong>Etape 1</strong> : S'asurer que le capteur est déjà connecté à un réseau en appuyant sur le bouton rouge et en attendant de voir les LED vertes clignotantes.</li>
+                      <li><strong>Etape 2</strong> : Renseigner les différents champs.</li>
+                      <li><strong>Etape 3</strong> : Appuyer sur envoyer les paramètres au serveur. Si le message Success s'affiche les paramètres sont bien renseignés sur le serveur.</li>
+                      <li><strong>Etape 4</strong> : Appuyer sur le bouton rouge pour permettre au capteur de récupérer les paramètres du réseau via le serveur.</li>
+                      <li><strong>Etape 5</strong> : Une fois les LED redevenues statiques, couper l'ancien réseau wi-fi et redémarrer le capteur pour qu'il se connecte au réseau nouvellement ajouté.</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            )}
 
-            {/* Server Response Display */}
+            {/* Affichage des champs et du QR Code selon le mode */}
+            {mode === 'generateQR' && (
+              <>
+                {/* SSID */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>SSID:</p>
+                    <input
+                      type="text"
+                      value={ssid}
+                      onChange={(e) => setSsid(e.target.value)}
+                      placeholder="Saisissez le SSID"
+                      style={{ width: '190px' }}
+                    />
+                  </div>
+                </div>
+
+                {/* Mot de passe */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>Mot de passe:</p>
+                    <input
+                      type="text"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Saisissez le mot de passe"
+                      style={{ width: '190px' }}
+                    />
+                  </div>
+                </div>
+
+                {/* Type de cryptage */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>Type de cryptage:</p>
+                    <select
+                      value={encryption}
+                      onChange={(e) => setEncryption(e.target.value)}
+                      style={{ width: '190px' }}
+                    >
+                      <option value="">Sélectionner</option>
+                      <option value="WPA">WPA</option>
+                      <option value="WEP">WEP</option>
+                      <option value="nopass">Aucun</option>
+                    </select>
+                  </div>
+                </div>
+
+                {/* Affichage du QR Code */}
+                <div className="w-fit p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <QRCode value={handleGenerateQR()} />
+                  </div>
+                </div>
+              </>
+            )}
+
+            {mode === 'enterNetwork' && (
+              <>
+                {/* ID Capteur */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>ID Capteur:</p>
+                    <input
+                      type="number"
+                      value={sensorId}
+                      onChange={(e) => setSensorId(e.target.value)}
+                      placeholder="Entrez l'ID du capteur"
+                      style={{ width: '190px' }}
+                    />
+                  </div>
+                </div>
+
+                {/* SSID */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>SSID:</p>
+                    <input
+                      type="text"
+                      value={ssid}
+                      onChange={(e) => setSsid(e.target.value)}
+                      placeholder="Saisissez le SSID"
+                      style={{ width: '190px' }}
+                    />
+                  </div>
+                </div>
+
+                {/* Mot de passe */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>Mot de passe:</p>
+                    <input
+                      type="text"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Saisissez le mot de passe"
+                      style={{ width: '190px' }}
+                    />
+                  </div>
+                </div>
+
+                {/* Type de cryptage */}
+                <div className="w-full sm:w-1/2 p-2 justify-center items-center ml-auto mr-auto">
+                  <div className="flex bg-white text-center rounded-lg shadow-xl border-2 mb-2 border-gray-400 p-2 justify-center items-center h-full">
+                    <p>Type de cryptage:</p>
+                    <select
+                      value={encryption}
+                      onChange={(e) => setEncryption(e.target.value)}
+                      style={{ width: '190px' }}
+                    >
+                      <option value="">Sélectionner</option>
+                      <option value="WPA">WPA</option>
+                      <option value="WEP">WEP</option>
+                      <option value="nopass">Aucun</option>
+                    </select>
+                  </div>
+                </div>
+
+                {/* Envoi des paramètres au serveur */}
+                <div className="w-fit p-2 justify-center items-center ml-auto mr-auto">
+                  <button
+                    onClick={handleSendToServer}
+                    className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md"
+                  >
+                    Envoyer les paramètres au serveur
+                  </button>
+                </div>
+              </>
+            )}
+
+            {/* Réponse du serveur */}
             {serverResponse && (
               <div className="w-full p-2 text-center text-red-500">
                 {serverResponse}
@@ -160,5 +301,4 @@ function Home() {
 
 Home.displayName = 'Home';
 
-// export default withAuth(Home);
 export default Home;
